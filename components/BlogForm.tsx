ab2ec298be8ebdaf75d3936400bc63019bcc1896
chirag <chirag@styava.com>
2024-04-22 22:56:57 +0530

    image and tag chnages

--- components/BlogForm.tsx
+++ components/BlogForm.tsx
@@ -8,14 +8,15 @@ import { Input } from "./ui/input";
 import { useCallback, useReducer, useState } from "react";
 import { redirect } from "next/navigation";
 import { useRouter } from "next/navigation";
-
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
 type Variants = "blog" | "project";
 type Inputs = {
     title: string;
     content: string;
     description: string;
-    tags: number;
+    tags: string[];
     cover_url: string;
 };
 interface BlogFormProps {
@@ -28,6 +29,8 @@ export default function BlogForm({
     id,
     value,
     variant = "blog",
+
+
 }: BlogFormProps) {
 
     const router = useRouter();
@@ -37,7 +40,8 @@ export default function BlogForm({
     }, {
         title: value?.title || '',
         content: value?.content || '',
-        description: value?.description || ''
+        description: value?.description || '',
+        tags: value?.tags || []
     })
 
     const updateContent = useCallback((data: editorProps) => {
@@ -72,6 +76,36 @@ export default function BlogForm({
             router.push("/admin/blogs");
         }
     }
+
+    const animatedComponents = makeAnimated();
+    const tags = [
+        {
+            label: 'HTML',
+            value: 'HTML',
+            name: 'HTML'
+        },
+        {
+            label: 'CSS',
+            value: 'CSS',
+            name: 'CSS'
+        },
+        {
+            label: 'JAVASCRIPT',
+            value: 'JAVASCRIPT',
+            name: 'JAVASCRIPT'
+        },
+        {
+            label: 'JAVA',
+            value: 'JAVA',
+            name: 'JAVA'
+        },
+        {
+            label: 'TYPESCRIPT',
+            value: 'TYPESCRIPT',
+            name: 'TYPESCRIPT'
+        },
+    ]
+
     return (
         <>
             <div>
@@ -88,14 +122,27 @@ export default function BlogForm({
             </div>
             <div className="mt-3 " draggable="false">
                 <Label htmlFor="">Content</Label>
-                <Editor
-                    editorProps={{
-
-                    }}
+                <Editor editorProps={{}}
                     onDebouncedUpdate={updateContent}
                     defaultValue={blogForm?.content}
                     className="border rounded pb-5"
                     disableLocalStorage
+                />
+            </div>
+            <div className="mt-3 " draggable="false">
+                <Label htmlFor="">Tags</Label>
+                <Select
+                    closeMenuOnSelect={false}
+                    components={animatedComponents}
+                    defaultValue={value?.tags.map(tag => ({ label: tag, value: tag, name: tag })) || []}
+                    isMulti
+                    options={tags.map(tag => ({ label: tag.label, value: tag.value, name: tag.name }))}
+                    onChange={(selectedTags) => {
+                        const tags = selectedTags.map(tag => tag.value); // Extract values from the array of selected objects
+                        setBlogForm({ tags: tags }); // Update tags in the state
+                    }}
+                    classNamePrefix={"select"}
+                    name="tags"
                 />
             </div>
             <div className="mt-4 flex float-right gap-3">
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "peqcditiycxknirditmt.supabase.co",
+                port: ""
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
